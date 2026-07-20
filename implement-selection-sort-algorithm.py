3807e649f41268e0a196c3b28ab49c3cb0ab9f20
ADDO Charles Kofi Owusu Junior <charlesaddo637@gmail.com>
2026-07-20 00:32:31 +0000

    Add files via upload

---
+++ implement-selection-sort-algorithm.py
@@ -0,0 +1,23 @@
+** start of main.py **
+
+def selection_sort(arr):
+    # Loop through each position in the list
+    for i in range(len(arr)):
+        # Assume the current position holds the minimum
+        min_index = i
+
+        # Find the index of the smallest element in the unsorted part
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap only if a smaller element was found
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    # Return the same list (sorted in-place)
+    return arr
+
+
+** end of main.py **
+
